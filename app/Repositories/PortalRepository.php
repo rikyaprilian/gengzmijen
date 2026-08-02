48ed11a4a13e79c77dd67ceab40c51c7d5064785
rikyaprilian <social.rikyap@gmail.com>
2026-08-02 19:54:24 +0700

    feat: implement homepage repository

--- app/Repositories/PortalRepository.php
+++ app/Repositories/PortalRepository.php
@@ -2,10 +2,30 @@
 
 namespace App\Repositories;
 
+use App\Models\Category;
+use App\Models\Setting;
+
 class PortalRepository
 {
     public function getHomepage(): array
     {
-        return [];
+        return [
+            'settings' => Setting::all()->pluck('value', 'key'),
+
+            'categories' => Category::query()
+                ->where('is_active', true)
+                ->with([
+                    'cards' => function ($query) {
+                        $query->where('is_active', true)
+                            ->orderBy('sort_order');
+                    },
+                    'cards.links' => function ($query) {
+                        $query->where('is_active', true)
+                            ->orderBy('sort_order');
+                    }
+                ])
+                ->orderBy('sort_order')
+                ->get(),
+        ];
     }
 }
