53129202fc7ffbf8429f546ca740b803bfc69c87
rikyaprilian <social.rikyap@gmail.com>
2026-08-02 17:38:40 +0700

    refactor: prepare project structure

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,16 @@
 // import './bootstrap';
 
 import '../css/app.css';
+import './bootstrap';
+// import '../css/app.css';
+
+import './portal';
+import './search';
+import './modal';
+import './manage';
+import './dragdrop';
 
 import * as bootstrap from 'bootstrap';
+
 
 window.bootstrap = bootstrap;
